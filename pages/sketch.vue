b6e5ac54cfe70d28cfaec6be19df028384ff0495
salernoelia <putopet@gmail.com>
2024-03-01 11:40:11 +0100

    p5 works

--- pages/sketch.vue
+++ pages/sketch.vue
@@ -4,29 +4,50 @@
 
 <script setup>
 import p5 from "p5";
+let oscData = ref();
+let gyroX = ref(0);
+let gyroY = ref(0);
+let gyroZ = ref(0);
+let gyroW = ref(0);
+
+let x, y, z;
+
+const fetchOSC = async () => {
+  oscData.value = await $fetch("/api/osc");
+  gyroX.value = oscData.value.oscData.args[0];
+  gyroY.value = oscData.value.oscData.args[1];
+  gyroZ.value = oscData.value.oscData.args[2];
+  gyroW.value = oscData.value.oscData.args[3];
+  console.log(gyroX.value, gyroY.value, gyroZ.value, gyroW.value);
+};
 
 let sketchContainer = ref(null);
 let sketchInstance;
 
 const setupSketch = () => {
-  sketchInstance = new p5((sketch) => {
-    sketch.setup = () => {
+  sketchInstance = new p5((s) => {
+    s.setup = () => {
       const { clientWidth, clientHeight } = sketchContainer.value;
-      sketch
-        .createCanvas(clientWidth, clientHeight)
-        .parent(sketchContainer.value);
+      s.createCanvas(clientWidth, clientHeight).parent(sketchContainer.value);
     };
 
-    sketch.draw = () => {
-      sketch.background(220);
-      sketch.ellipse(sketch.width / 2, sketch.height / 2, 200, 200);
+    s.draw = () => {
+      s.x = s.map(gyroX.value, -1, 1, 0, s.width);
+      s.y = s.map(-gyroY.value, -1, 1, 0, s.height);
+
+      s.background(220);
+      s.ellipse(s.x, s.y, 200, 200);
     };
-  }, sketchContainer.value);
+  });
 };
 
 onMounted(async () => {
   setupSketch();
+  fetchOSC();
 });
+
+// Set up an interval to fetch data every second
+const timer = setInterval(fetchOSC, 100);
 </script>
 
 <style scoped>
