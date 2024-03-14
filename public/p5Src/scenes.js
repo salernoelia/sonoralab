2b5fab1e7e5e04e2f982bde1f380237adaa4ba9d
salernoelia <putopet@gmail.com>
2024-03-14 12:36:57 +0100

    fix: stability until scene 3

--- public/p5Src/scenes.js
+++ public/p5Src/scenes.js
@@ -1,5 +1,6 @@
 function scene1() {
-  if ((xR && yR) || (xL && yL)) {
+  image(img1, 0, 0, window.innerWidth, window.innerHeight);
+  if (handsVisible.value === true) {
     sceneMap.set("scene1", true);
 
     timer1 = setTimeout(function () {
@@ -7,25 +8,8 @@ function scene1() {
       timer1 = millis();
     }, 3000);
   }
-  image(img1, 0, 0, window.innerWidth, window.innerHeight);
 }
 function scene2() {
-  if (resetTimer === false) {
-    startTimer();
-    resetTimer = true;
-  }
-  if (millis() - startTime > 15000 && checkConditions() === false) {
-    mode = 1;
-  } else if (checkConditions() === true) {
-    sceneMap.set("scene2", true);
-    resetTimer = false;
-    startTime = 0;
-    timer1 = millis();
-    mode = 3;
-  }
-  console.log("scene2 elapsed kack", elapsedTime);
-  console.log("scene2 conditionMetTime", conditionMetTime);
-
   image(img2, 0, 0, window.innerWidth, window.innerHeight);
   fill(255);
   textAlign(CENTER, CENTER);
@@ -36,22 +20,34 @@ function scene2() {
   let str = elapsedTime.toFixed(1);
   text(str, window.innerWidth / 2, window.innerHeight / 1.1);
 
-  ellipse;
+  //  ellipse;
+
+  if (resetTimer === false) {
+    startTimer();
+    resetTimer = true;
+  }
+  if (checkConditions() === true) {
+    sceneMap.set("scene2", true);
+    resetTimer = false;
+    startTime = 0;
+    timer1 = millis();
+    mode = 3;
+  } else if (millis() - startTime > 15000 && checkConditions() === false) {
+    mode = 1;
+  }
 }
 function scene3() {
-  timer1 = setTimeout(function () {
+  background(0);
+
+  if (recorderStarted === false) {
     Tone.Transport.start();
     Tone.Transport.scheduleRepeat(setMelody, "4n");
-    if (recorderStarted === false) {
-      recorder.start();
-      console.log("Recording");
-      recorderStarted = true;
-      sceneMap.set("scene3", true);
-
-      mode = 4;
-    }
-  }, 30);
-  background(0);
+    recorder.start();
+    console.log("Recording");
+    recorderStarted = true;
+    sceneMap.set("scene3", true);
+    mode = 4;
+  }
 }
 
 function scene4() {
